c8dc0712712d52aa3116be8a66fdefff709965a4
Happu2 <happu20022@gmail.com>
2025-04-22 12:02:03 +0530

    first commit

---
+++ spring-boot-and-kafka/src/main/java/project/spring/boot/and/kafka/SpringBootAndKafkaApplication.java
@@ -0,0 +1,13 @@
+package project.spring.boot.and.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAndKafkaApplication.class, args);
+	}
+
+}
---
+++ spring-boot-and-kafka/src/test/java/project/spring/boot/and/kafka/SpringBootAndKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package project.spring.boot.and.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAndKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
